1479e61ee19bd187a7a14bd24275a79a50d54808
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-07-16 13:10:41 +0800

    added nextjs static generation to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import GameComponent from '../components/game_component'
-import styles from '../styles/Home.module.css'
+
+export async function getStaticProps() {
+  return {
+    props: {}
+  }
+}
 
 const Home: NextPage = () => {
   return (
