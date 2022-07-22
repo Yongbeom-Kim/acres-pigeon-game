dbc3fbb3ecb1af2de7928be31559cfa1d17ff7e0
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-07-22 14:47:37 +0800

    added placeholder background start screen

--- components/game/game_logic/scenes/start_scene/StartScene.ts
+++ components/game/game_logic/scenes/start_scene/StartScene.ts
@@ -1,5 +1,7 @@
-import start_game_button_spritesheet from './assets/placeholder_start_button.png';
-import start_game_button_json from './assets/placeholder_start_button.json' assert {type: 'json'};
+// We need these import statements to tell NextJS to build the pages together with this page.
+import background from './assets/background_600x800.png';
+import start_game_button_spritesheet from './assets/start_button_spritesheet.png';
+import start_game_button_json from './assets/start_button_spritesheet.json' assert {type: 'json'};
 
 export default class SampleScene extends Phaser.Scene {
 
@@ -12,17 +14,27 @@ export default class SampleScene extends Phaser.Scene {
     text!: Phaser.GameObjects.Text;
 
     preload() {
+        this.load.image('background', background.src)
         this.load.spritesheet('start_game_button', start_game_button_spritesheet.src, start_game_button_json)
     }
 
     create() {
+        // Background, and stretch background to fit screen.
+        const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        background.setScale(Math.max(
+            this.cameras.main.width / background.width,
+            this.cameras.main.height / background.height
+        ));
 
-        const start_game_button = this.add.sprite(this.scale.width/2, this.scale.height/2, 'start_game_button');
-        // start_game_button.setPosition(this.scale.width/2, this.scale.height/2)  m ,
+        const start_game_button = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'start_game_button');
         start_game_button.setInteractive();
 
+        // Make button interactive
         start_game_button.on('pointerover', () => start_game_button.setFrame(1));
         start_game_button.on('pointerout', () => start_game_button.setFrame(0));
+
+
+
     }
 
     update() {
